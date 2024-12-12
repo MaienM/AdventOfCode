07a9350953f11d5d6ee98de91968be19de3bf9f0
Michon van Dooren <me@maienm.com>
2024-12-12 07:26:40 +0100

    Implement 24-12-2

--- src/bin/24-12.rs
+++ src/bin/24-12.rs
@@ -1,17 +1,11 @@
 use std::collections::HashSet;
 
-use aoc::utils::{parse, point::Point2};
+use aoc::utils::{
+    parse,
+    point::{Direction2, Point2},
+};
 
-#[derive(Debug, Eq, PartialEq)]
-struct Region {
-    size: usize,
-    perimeter: usize,
-}
-impl Region {
-    fn price(&self) -> usize {
-        self.size * self.perimeter
-    }
-}
+type Region = HashSet<Point2>;
 
 fn parse_input(input: &str) -> Vec<Vec<char>> {
     parse!(input => { [tiles split on '\n' with [chars]] } => tiles)
@@ -41,36 +35,80 @@ fn find_regions(tiles: &[Vec<char>]) -> Vec<Region> {
             if !visited.contains(&point) {
                 let mut members = HashSet::new();
                 members.insert(point);
-                let chr = tiles[point.y][point.x];
-                find_region(tiles, &mut members, &point, chr);
-
+                find_region(tiles, &mut members, &point, tiles[point.y][point.x]);
                 visited.extend(members.clone());
-
-                let perimeter = members
-                    .iter()
-                    .map(|member| {
-                        4 - member
-                            .neighbours_ortho()
-                            .into_iter()
-                            .filter(|n| members.contains(n))
-                            .count()
-                    })
-                    .sum();
-
-                regions.push(Region {
-                    size: members.len(),
-                    perimeter,
-                });
+                regions.push(members);
             }
         }
     }
     regions
 }
 
+fn price_normal(region: &Region) -> usize {
+    let perimeter: usize = region
+        .iter()
+        .map(|member| {
+            4 - member
+                .neighbours_ortho()
+                .into_iter()
+                .filter(|n| region.contains(n))
+                .count()
+        })
+        .sum();
+    perimeter * region.len()
+}
+
+fn has_neighbour_ortho(region: &Region, point: &Point2, direction: Direction2) -> bool {
+    point
+        .checked_add_direction(direction, &1)
+        .is_some_and(|p| region.contains(&p))
+}
+
+fn has_neighbour_diag(region: &Region, point: &Point2, direction: [Direction2; 2]) -> bool {
+    point
+        .checked_add_direction(direction[0], &1)
+        .and_then(|p| p.checked_add_direction(direction[1], &1))
+        .is_some_and(|p| region.contains(&p))
+}
+
+fn price_bulk(region: &Region) -> usize {
+    // We're just counting the corners here, because the amount of sides equals the amount of corners.
+    let sides: usize = region
+        .iter()
+        .map(|member| {
+            let n = has_neighbour_ortho(region, member, Direction2::North);
+            let e = has_neighbour_ortho(region, member, Direction2::East);
+            let s = has_neighbour_ortho(region, member, Direction2::South);
+            let w = has_neighbour_ortho(region, member, Direction2::West);
+            let ne = has_neighbour_diag(region, member, [Direction2::North, Direction2::East]);
+            let nw = has_neighbour_diag(region, member, [Direction2::North, Direction2::West]);
+            let se = has_neighbour_diag(region, member, [Direction2::South, Direction2::East]);
+            let sw = has_neighbour_diag(region, member, [Direction2::South, Direction2::West]);
+            // Outside corner.
+            usize::from(!n && !e)
+                + usize::from(!e && !s)
+                + usize::from(!s && !w)
+                + usize::from(!w && !n)
+                // Inside corner.
+                + usize::from(n && e && !ne)
+                + usize::from(n && w && !nw)
+                + usize::from(s && e && !se)
+                + usize::from(s && w && !sw)
+        })
+        .sum();
+    sides * region.len()
+}
+
 pub fn part1(input: &str) -> usize {
     let tiles = parse_input(input);
     let regions = find_regions(&tiles);
-    regions.into_iter().map(|r| r.price()).sum()
+    regions.iter().map(price_normal).sum()
+}
+
+pub fn part2(input: &str) -> usize {
+    let tiles = parse_input(input);
+    let regions = find_regions(&tiles);
+    regions.iter().map(price_bulk).sum()
 }
 
 aoc::cli::single::generate_main!();
@@ -82,7 +120,7 @@ mod tests {
 
     use super::*;
 
-    #[example_input(part1 = 140)]
+    #[example_input(part1 = 140, part2 = 80)]
     static EXAMPLE_INPUT_1: &str = "
         AAAA
         BBCD
@@ -111,6 +149,25 @@ mod tests {
         MIIIIIJJEE
         MIIISIJEEE
         MMMISSJEEE
+    ";
+
+    #[example_input(part2 = 236)]
+    static EXAMPLE_INPUT_4: &str = "
+        EEEEE
+        EXXXX
+        EEEEE
+        EXXXX
+        EEEEE
+    ";
+
+    #[example_input(part2 = 368)]
+    static EXAMPLE_INPUT_5: &str = "
+        AAAAAA
+        AAABBA
+        AAABBA
+        ABBAAA
+        ABBAAA
+        AAAAAA
     ";
 
     // #[test]
