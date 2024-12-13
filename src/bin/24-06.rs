f2eaa01a9c4c907110c6ee47f0d7766cafbeaa35
Michon van Dooren <me@maienm.com>
2024-12-13 20:45:51 +0100

    Optimize 24-06

--- src/bin/24-06.rs
+++ src/bin/24-06.rs
@@ -1,6 +1,7 @@
 use std::collections::HashSet;
 
 use aoc::utils::point::{Direction2, Point2};
+use rayon::prelude::*;
 
 type Point = Point2<usize>;
 
@@ -27,79 +28,121 @@ fn parse_input(input: &str) -> (Point, HashSet<Point>, Point) {
     (guard.unwrap(), obstacles, bounds)
 }
 
-fn get_path_out(guard: &Point, obstacles: &HashSet<Point>, bounds: &Point) -> HashSet<Point> {
-    let mut visited = HashSet::new();
-    visited.insert(*guard);
-    let mut guard = (*guard, Direction2::North);
-    while let Some(nextpoint) = guard.0.checked_add_direction(guard.1, &1) {
-        if obstacles.contains(&nextpoint) {
-            guard.1 = match guard.1 {
-                Direction2::North => Direction2::East,
-                Direction2::East => Direction2::South,
-                Direction2::South => Direction2::West,
-                Direction2::West => Direction2::North,
-            };
-            continue;
-        }
-
-        if nextpoint.x >= bounds.x || nextpoint.y >= bounds.y {
-            break;
-        }
-        visited.insert(nextpoint);
-        guard.0 = nextpoint;
+#[inline]
+fn get_next_direction(direction: Direction2) -> Direction2 {
+    match direction {
+        Direction2::North => Direction2::East,
+        Direction2::East => Direction2::South,
+        Direction2::South => Direction2::West,
+        Direction2::West => Direction2::North,
     }
+}
+
+fn get_next_obstacle<'a>(
+    guard: &Point,
+    direction: Direction2,
+    obstacles: &'a [&'a Point],
+    extra_obstacle: Option<&'a &'a Point>,
+) -> Option<&'a &'a Point> {
+    match direction {
+        Direction2::North => obstacles
+            .iter()
+            .chain(extra_obstacle)
+            .filter(|p| p.x == guard.x && p.y < guard.y)
+            .max_by_key(|p| p.y),
+        Direction2::South => obstacles
+            .iter()
+            .chain(extra_obstacle)
+            .filter(|p| p.x == guard.x && p.y > guard.y)
+            .min_by_key(|p| p.y),
+        Direction2::East => obstacles
+            .iter()
+            .chain(extra_obstacle)
+            .filter(|p| p.y == guard.y && p.x > guard.x)
+            .min_by_key(|p| p.x),
+        Direction2::West => obstacles
+            .iter()
+            .chain(extra_obstacle)
+            .filter(|p| p.y == guard.y && p.x < guard.x)
+            .max_by_key(|p| p.x),
+    }
+}
+
+fn add_path_to_visited(
+    visited: &mut HashSet<Point>,
+    guard: &Point,
+    direction: Direction2,
+    obstacle: &Point,
+) {
+    match direction {
+        Direction2::North => {
+            visited.extend(((obstacle.y + 1)..=guard.y).map(|y| Point::new(guard.x, y)));
+        }
+        Direction2::South => {
+            visited.extend((guard.y..obstacle.y).map(|y| Point::new(guard.x, y)));
+        }
+        Direction2::West => {
+            visited.extend(((obstacle.x + 1)..=guard.x).map(|x| Point::new(x, guard.y)));
+        }
+        Direction2::East => {
+            visited.extend((guard.x..obstacle.x).map(|x| Point::new(x, guard.y)));
+        }
+    }
+}
+
+fn get_path_out(mut guard: Point, obstacles: &HashSet<Point>, bounds: &Point) -> HashSet<Point> {
+    let mut visited = HashSet::new();
+    visited.insert(guard);
+    let obstacles: Vec<_> = obstacles.iter().collect();
+    let mut direction = Direction2::North;
+
+    while let Some(obstacle) = get_next_obstacle(&guard, direction, &obstacles, None) {
+        add_path_to_visited(&mut visited, &guard, direction, obstacle);
+        guard = **obstacle - direction;
+        direction = get_next_direction(direction);
+    }
+
+    let edge = match direction {
+        Direction2::North | Direction2::West => {
+            let edge = Point::new(0, 0);
+            // The add_path_to_visited doesn't include the 'obstacle', but since we're using unsigned integers for the coordinated we can't position an obstacle past the edge like that, so we just add it to the visited list directly here.
+            visited.insert(edge);
+            edge
+        }
+        Direction2::East | Direction2::South => *bounds,
+    };
+    add_path_to_visited(&mut visited, &guard, direction, &edge);
+
     visited
 }
 
-fn check_loop(
-    guard: &Point,
-    obstacles: &HashSet<Point>,
-    extra_obstacle: &Point,
-    bounds: &Point,
-) -> bool {
+fn check_loop(mut guard: Point, obstacles: &HashSet<Point>, extra_obstacle: &Point) -> bool {
     let mut bumped = HashSet::new();
-    let mut guard = (*guard, Direction2::North);
-    while let Some(nextpoint) = guard.0.checked_add_direction(guard.1, &1) {
-        if obstacles.contains(&nextpoint) || &nextpoint == extra_obstacle {
-            if !bumped.insert((nextpoint, guard.1)) {
-                return true;
-            }
-            guard.1 = match guard.1 {
-                Direction2::North => Direction2::East,
-                Direction2::East => Direction2::South,
-                Direction2::South => Direction2::West,
-                Direction2::West => Direction2::North,
-            };
-            continue;
+    let mut direction = Direction2::North;
+    let obstacles: Vec<_> = obstacles.iter().collect();
+    while let Some(obstacle) =
+        get_next_obstacle(&guard, direction, &obstacles, Some(&extra_obstacle))
+    {
+        if !bumped.insert((obstacle, direction)) {
+            return true;
         }
-
-        if nextpoint.x > bounds.x || nextpoint.y > bounds.y {
-            return false;
-        }
-        guard.0 = nextpoint;
+        guard = **obstacle - direction;
+        direction = get_next_direction(direction);
     }
     false
 }
 
 pub fn part1(input: &str) -> usize {
     let (guard, obstacles, bounds) = parse_input(input);
-    let visited = get_path_out(&guard, &obstacles, &bounds);
-    visited.len()
+    get_path_out(guard, &obstacles, &bounds).len()
 }
 
 pub fn part2(input: &str) -> usize {
     let (guard, obstacles, bounds) = parse_input(input);
-    let visited = get_path_out(&guard, &obstacles, &bounds);
-    let mut loops = 0;
-    for point in visited {
-        if point == guard {
-            continue;
-        }
-        if check_loop(&guard, &obstacles, &point, &bounds) {
-            loops += 1;
-        }
-    }
-    loops
+    get_path_out(guard, &obstacles, &bounds)
+        .into_par_iter()
+        .filter(|point| check_loop(guard, &obstacles, point))
+        .count()
 }
 
 aoc::cli::single::generate_main!();
