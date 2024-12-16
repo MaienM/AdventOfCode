4752a49a5679690cebe4d5b9dc224987a0369f30
Michon van Dooren <me@maienm.com>
2024-12-16 22:49:06 +0100

    Implement 24-16-2

--- src/bin/24-16.rs
+++ src/bin/24-16.rs
@@ -1,4 +1,4 @@
-use std::collections::{BinaryHeap, HashSet};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
 use aoc::utils::point::{Direction2, Point2};
 
@@ -65,9 +65,64 @@ fn find_path(map: &Map, start: &Point2, end: &Point2) -> usize {
     }
 }
 
+fn find_best_path_tiles(map: &Map, start: &Point2, end: &Point2) -> usize {
+    let mut paths = BinaryHeap::new();
+    let mut done = HashMap::new();
+    paths.push((0, *start, Direction2::East, vec![*start]));
+    let mut best_score = None;
+    let mut tiles: HashSet<Point2> = HashSet::new();
+    loop {
+        let Some((score, point, facing, mut path)) = paths.pop() else {
+            break;
+        };
+
+        if let Some(best_score) = best_score {
+            if score == best_score && point == *end {
+                tiles.extend(path);
+                continue;
+            } else if score < best_score {
+                break;
+            }
+        } else if point == *end {
+            best_score = Some(score);
+            tiles.extend(path);
+            continue;
+        }
+
+        if done.get(&(point, facing)).is_some_and(|v| *v > score) {
+            continue;
+        }
+        done.insert((point, facing), score);
+
+        match facing {
+            Direction2::North | Direction2::South => {
+                paths.push((score - 1000, point, Direction2::East, path.clone()));
+                paths.push((score - 1000, point, Direction2::West, path.clone()));
+            }
+            Direction2::East | Direction2::West => {
+                paths.push((score - 1000, point, Direction2::North, path.clone()));
+                paths.push((score - 1000, point, Direction2::South, path.clone()));
+            }
+        }
+
+        let next = point + facing;
+        if map[next.y][next.x] {
+            path.push(next);
+            paths.push((score - 1, next, facing, path));
+        }
+    }
+
+    tiles.len()
+}
+
 pub fn part1(input: &str) -> usize {
     let (map, start, end) = parse_input(input);
     find_path(&map, &start, &end)
+}
+
+pub fn part2(input: &str) -> usize {
+    let (map, start, end) = parse_input(input);
+    find_best_path_tiles(&map, &start, &end)
 }
 
 aoc::cli::single::generate_main!();
@@ -79,7 +134,7 @@ mod tests {
 
     use super::*;
 
-    #[example_input(part1 = 7036)]
+    #[example_input(part1 = 7036, part2 = 45)]
     static EXAMPLE_INPUT_1: &str = "
         ###############
         #.......#....E#
@@ -98,7 +153,7 @@ mod tests {
         ###############
     ";
 
-    #[example_input(part1 = 11_048)]
+    #[example_input(part1 = 11_048, part2 = 64)]
     static EXAMPLE_INPUT_2: &str = "
         #################
         #...#...#...#..E#
@@ -119,10 +174,75 @@ mod tests {
         #################
     ";
 
-    // #[test]
-    // fn example_parse() {
-    //     let actual = parse_input(&EXAMPLE_INPUT);
-    //     let expected = vec![1, 2];
-    //     assert_eq!(actual, expected);
-    // }
+    #[test]
+    fn example_parse() {
+        let actual = parse_input(&EXAMPLE_INPUT_1);
+        let expected = (
+            vec![
+                vec![
+                    false, false, false, false, false, false, false, false, false, false, false,
+                    false, false, false, false,
+                ],
+                vec![
+                    false, true, true, true, true, true, true, true, false, true, true, true, true,
+                    true, false,
+                ],
+                vec![
+                    false, true, false, true, false, false, false, true, false, true, false, false,
+                    false, true, false,
+                ],
+                vec![
+                    false, true, true, true, true, true, false, true, false, true, true, true,
+                    false, true, false,
+                ],
+                vec![
+                    false, true, false, false, false, true, false, false, false, false, false,
+                    true, false, true, false,
+                ],
+                vec![
+                    false, true, false, true, false, true, true, true, true, true, true, true,
+                    false, true, false,
+                ],
+                vec![
+                    false, true, false, true, false, false, false, false, false, true, false,
+                    false, false, true, false,
+                ],
+                vec![
+                    false, true, true, true, true, true, true, true, true, true, true, true, false,
+                    true, false,
+                ],
+                vec![
+                    false, false, false, true, false, true, false, false, false, false, false,
+                    true, false, true, false,
+                ],
+                vec![
+                    false, true, true, true, false, true, true, true, true, true, false, true,
+                    false, true, false,
+                ],
+                vec![
+                    false, true, false, true, false, true, false, false, false, true, false, true,
+                    false, true, false,
+                ],
+                vec![
+                    false, true, true, true, true, true, false, true, true, true, false, true,
+                    false, true, false,
+                ],
+                vec![
+                    false, true, false, false, false, true, false, true, false, true, false, true,
+                    false, true, false,
+                ],
+                vec![
+                    false, true, true, true, false, true, true, true, true, true, false, true,
+                    true, true, false,
+                ],
+                vec![
+                    false, false, false, false, false, false, false, false, false, false, false,
+                    false, false, false, false,
+                ],
+            ],
+            Point2::new(1, 13),
+            Point2::new(13, 1),
+        );
+        assert_eq!(actual, expected);
+    }
 }
