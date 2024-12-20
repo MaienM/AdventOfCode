0a83c91b7d78208c1e78290a92d78d778208d85b
Michon van Dooren <me@maienm.com>
2024-12-20 07:18:01 +0100

    Implement 24-20-2

--- src/bin/24-20.rs
+++ src/bin/24-20.rs
@@ -1,6 +1,6 @@
 use std::collections::{BinaryHeap, HashSet};
 
-use aoc::utils::point::{Direction2, Point2};
+use aoc::utils::point::Point2;
 use rayon::prelude::*;
 
 type Map = Vec<Vec<bool>>;
@@ -35,51 +35,80 @@ fn parse_input(input: &str) -> (Map, Point2, Point2) {
 
 fn find_path(map: &Map, start: &Point2, end: &Point2) -> usize {
     let mut paths = BinaryHeap::new();
-    let mut done = HashSet::new();
+    let mut visited = HashSet::new();
     paths.push((0, *start));
     loop {
-        let (score, point) = paths.pop().unwrap();
+        let (steps, point) = paths.pop().unwrap();
 
         if point == *end {
-            return -score as usize;
+            return -steps as usize;
         }
 
-        if !done.insert(point) || !map[point.y][point.x] {
+        if !visited.insert(point) || !map[point.y][point.x] {
             continue;
         }
 
-        paths.push((score - 1, point + Direction2::East));
-        paths.push((score - 1, point + Direction2::West));
-        paths.push((score - 1, point + Direction2::North));
-        paths.push((score - 1, point + Direction2::South));
+        for neigh in point.neighbours_ortho() {
+            paths.push((steps - 1, neigh));
+        }
     }
 }
 
-pub fn part1(input: &str) -> usize {
-    let (map, start, end) = parse_input(input);
+fn find_cheat_paths(
+    map: &Map,
+    start: &Point2,
+    end: &Point2,
+    cheat: usize,
+    min_save: usize,
+) -> usize {
     let bounds = Point2::new(map[0].len(), map.len());
-    let base = find_path(&map, &start, &end);
-    (1..(bounds.x - 1))
-        .into_par_iter()
-        .map(|x| {
+    let threshold = find_path(map, start, end) - min_save;
+    let non_wall_points: Vec<_> = (1..(bounds.x - 1))
+        .flat_map(|x| {
             (1..(bounds.y - 1))
-                .into_par_iter()
-                .filter(|y| {
-                    let y = *y;
-                    if !map[y][x]
-                        && ((map[y - 1][x] && map[y + 1][x]) || (map[y][x - 1] && map[y][x + 1]))
-                    {
-                        let mut map = map.clone();
-                        map[y][x] = true;
-                        let with_cheating = find_path(&map, &start, &end);
-                        (base - with_cheating) >= 100
-                    } else {
-                        false
-                    }
+                .filter(|y| map[*y][x])
+                .map(|y| Point2::new(x, y))
+                .collect::<Vec<_>>()
+        })
+        .collect();
+    let from_start: Vec<_> = non_wall_points
+        .par_iter()
+        .map(|to| (to, find_path(map, start, to)))
+        .collect();
+    let from_end: Vec<_> = non_wall_points
+        .par_iter()
+        .map(|to| (to, find_path(map, end, to)))
+        .collect();
+    from_start
+        .par_iter()
+        .map(|(point1, steps1)| {
+            from_end
+                .iter()
+                .filter(|(point2, steps2)| {
+                    let distance = point1.abs_diff(point2).sum();
+                    distance <= cheat && steps1 + distance + steps2 <= threshold
                 })
                 .count()
         })
         .sum()
+}
+
+fn part1impl(input: &str, min_save: usize) -> usize {
+    let (map, start, end) = parse_input(input);
+    find_cheat_paths(&map, &start, &end, 2, min_save)
+}
+
+pub fn part1(input: &str) -> usize {
+    part1impl(input, 100)
+}
+
+fn part2impl(input: &str, min_save: usize) -> usize {
+    let (map, start, end) = parse_input(input);
+    find_cheat_paths(&map, &start, &end, 20, min_save)
+}
+
+pub fn part2(input: &str) -> usize {
+    part2impl(input, 100)
 }
 
 aoc::cli::single::generate_main!();
@@ -91,7 +120,7 @@ mod tests {
 
     use super::*;
 
-    #[example_input(part1 = 1, notest)]
+    #[example_input(part1 = 44, part2 = 285, notest)]
     static EXAMPLE_INPUT: &str = "
         ###############
         #...#...#.....#
@@ -110,10 +139,15 @@ mod tests {
         ###############
     ";
 
-    // #[test]
-    // fn example_parse() {
-    //     let actual = parse_input(&EXAMPLE_INPUT);
-    //     let expected = vec![1, 2];
-    //     assert_eq!(actual, expected);
-    // }
+    #[test]
+    fn example_test_1() {
+        let actual = part1impl(&EXAMPLE_INPUT, 1).to_string();
+        assert_eq!(actual, EXAMPLE_INPUT.part1.unwrap());
+    }
+
+    #[test]
+    fn example_test_2() {
+        let actual = part2impl(&EXAMPLE_INPUT, 50).to_string();
+        assert_eq!(actual, EXAMPLE_INPUT.part2.unwrap());
+    }
 }
