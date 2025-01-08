cc305335b882a63fd27674ab7288f69bd9721520
Michon van Dooren <me@maienm.com>
2025-01-08 17:58:02 +0100

    Make PartitionPoint work for more int types

--- puzzle_lib/src/ext/partition_point.rs
+++ puzzle_lib/src/ext/partition_point.rs
@@ -1,4 +1,6 @@
-use std::ops::{Add, Bound, Div, RangeBounds, Sub};
+use std::ops::{Bound, RangeBounds};
+
+use num::PrimInt;
 
 /// Find the partition point of a collection, as [`slice::partition_point`].
 pub trait PartitionPoint<T> {
@@ -12,13 +14,7 @@ pub trait PartitionPoint<T> {
 impl<R, T> PartitionPoint<T> for R
 where
     R: RangeBounds<T>,
-    T: Add<usize, Output = T>
-        + Add<T, Output = T>
-        + Sub<usize, Output = T>
-        + Sub<T, Output = T>
-        + Div<usize, Output = T>
-        + Copy
-        + PartialEq,
+    T: PrimInt,
 {
     fn partition_point<F>(&self, f: F) -> Option<T>
     where
@@ -26,20 +22,20 @@ where
     {
         let mut min = match self.start_bound() {
             Bound::Included(n) => *n,
-            Bound::Excluded(n) => *n + 1,
-            Bound::Unbounded => panic!("Cannot do binary search on an unbounded range."),
+            Bound::Excluded(n) => *n + T::one(),
+            Bound::Unbounded => panic!("Cannot find partition point of an unbounded range."),
         };
         let mut max = match self.end_bound() {
             Bound::Included(n) => *n,
-            Bound::Excluded(n) => *n - 1,
-            Bound::Unbounded => panic!("Cannot do binary search on an unbounded range."),
+            Bound::Excluded(n) => *n - T::one(),
+            Bound::Unbounded => panic!("Cannot find partition point of an unbounded range."),
         };
         while min != max {
-            let midpoint = min + (max - min) / 2;
+            let midpoint = min + ((max - min) >> 1);
             if f(midpoint) {
                 max = midpoint;
             } else {
-                min = midpoint + 1;
+                min = midpoint + T::one();
             }
         }
         if f(min) {
@@ -58,22 +54,17 @@ mod tests {
 
     #[test]
     fn partition_point() {
-        assert_eq!((1..10).partition_point(|v| v > 6), Some(7));
-        assert_eq!(
-            (1..1_000_000_000).partition_point(|v| v > 628_162_832),
-            Some(628_162_833)
-        );
-        assert_eq!((1..10).partition_point(|v| v > 0), Some(1));
+        assert_eq!((1..10).partition_point(|_| true), Some(1));
         assert_eq!((1..10).partition_point(|v| v > 8), Some(9));
         assert_eq!((1..10).partition_point(|v| v > 9), None);
 
-        assert_eq!((1..=10).partition_point(|v| v > 6), Some(7));
+        assert_eq!((1..=10).partition_point(|_| true), Some(1));
+        assert_eq!((1..=10).partition_point(|v| v > 9), Some(10));
+        assert_eq!((1..=10).partition_point(|v| v > 10), None);
+
         assert_eq!(
             (1..1_000_000_000).partition_point(|v| v > 628_162_832),
             Some(628_162_833)
         );
-        assert_eq!((1..=10).partition_point(|v| v > 0), Some(1));
-        assert_eq!((1..=10).partition_point(|v| v > 9), Some(10));
-        assert_eq!((1..=10).partition_point(|v| v > 10), None);
     }
 }
