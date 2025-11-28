6cb249256daa5575e1ccbc0ef8c092401950fdd6
Michon van Dooren <me@maienm.com>
2025-11-28 22:34:57 +0100

    Implement 15-03-1

---
+++ aoc/src/bin/15-03.rs
@@ -0,0 +1,57 @@
+puzzle_lib::setup!(title = "Perfectly Spherical Houses in a Vacuum");
+
+use std::collections::HashSet;
+
+use puzzle_lib::point::{Direction2, Point2};
+
+fn parse_input(input: &str) -> Vec<Direction2> {
+    parse!(input => {
+        [directions chars match {
+            '^' => Direction2::North,
+            '>' => Direction2::East,
+            'v' => Direction2::South,
+            '<' => Direction2::West,
+        }]
+    } => directions)
+}
+
+pub fn part1(input: &str) -> usize {
+    let directions = parse_input(input);
+    let mut visited = HashSet::new();
+    let mut current = Point2::new(0, 0);
+    visited.insert(current);
+    for direction in directions {
+        current += direction;
+        visited.insert(current);
+    }
+    visited.len()
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+    use puzzle_runner::example_input;
+
+    use super::*;
+
+    #[example_input(part1 = 2)]
+    static EXAMPLE_INPUT_1: &str = ">";
+
+    #[example_input(part1 = 4)]
+    static EXAMPLE_INPUT_2: &str = "^>v<";
+
+    #[example_input(part1 = 2)]
+    static EXAMPLE_INPUT_3: &str = "^v^v^v^v^v";
+
+    #[test]
+    fn example_parse() {
+        let actual = parse_input(&EXAMPLE_INPUT_2);
+        let expected = vec![
+            Direction2::North,
+            Direction2::East,
+            Direction2::South,
+            Direction2::West,
+        ];
+        assert_eq!(actual, expected);
+    }
+}
