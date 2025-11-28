5cb7fd2d9929768d6befbc64efbc3c4ec8659997
Michon van Dooren <me@maienm.com>
2025-11-28 23:46:04 +0100

    Implement 15-04-2

--- aoc/src/bin/15-04.rs
+++ aoc/src/bin/15-04.rs
@@ -15,6 +15,19 @@ pub fn part1(input: &str) -> usize {
         .unwrap()
 }
 
+pub fn part2(input: &str) -> usize {
+    (0..usize::MAX)
+        .into_par_iter()
+        .by_exponential_blocks()
+        .find_first(|n| {
+            let mut hasher = Md5::new();
+            hasher.update(format!("{input}{n}"));
+            let hash = hasher.finalize();
+            hash[0] == 0 && hash[1] == 0 && hash[2] == 0
+        })
+        .unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use pretty_assertions::assert_eq;
