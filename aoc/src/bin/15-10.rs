1c7bb682f50f34b89d4754bc9a9d4a878ac10a4d
Michon van Dooren <me@maienm.com>
2025-11-30 18:41:18 +0100

    Implement 15-10-1

---
+++ aoc/src/bin/15-10.rs
@@ -0,0 +1,59 @@
+puzzle_lib::setup!(title = "Elves Look, Elves Say");
+
+fn parse_input(input: &str) -> Vec<u8> {
+    parse!(input => { [nums chars as u8] } => nums)
+}
+
+fn look_and_say(nums: Vec<u8>) -> Vec<u8> {
+    let mut result = Vec::new();
+    let mut iter = nums.into_iter();
+
+    let mut current = iter.next().unwrap();
+    let mut count = 1;
+    for next in iter {
+        if next == current {
+            count += 1;
+        } else {
+            result.extend([count, current]);
+            current = next;
+            count = 1;
+        }
+    }
+    result.extend([count, current]);
+    result
+}
+
+pub fn part1(input: &str) -> usize {
+    let mut nums = parse_input(input);
+    for _ in 0..40 {
+        nums = look_and_say(nums);
+    }
+    nums.len()
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+    use puzzle_runner::example_input;
+
+    use super::*;
+
+    #[example_input]
+    static EXAMPLE_INPUT: &str = "1";
+
+    #[test]
+    fn example_times_5() {
+        let mut nums = parse_input(&EXAMPLE_INPUT);
+        assert_eq!(nums, vec![1]);
+        nums = look_and_say(nums);
+        assert_eq!(nums, vec![1, 1]);
+        nums = look_and_say(nums);
+        assert_eq!(nums, vec![2, 1]);
+        nums = look_and_say(nums);
+        assert_eq!(nums, vec![1, 2, 1, 1]);
+        nums = look_and_say(nums);
+        assert_eq!(nums, vec![1, 1, 1, 2, 2, 1]);
+        nums = look_and_say(nums);
+        assert_eq!(nums, vec![3, 1, 2, 2, 1, 1]);
+    }
+}
