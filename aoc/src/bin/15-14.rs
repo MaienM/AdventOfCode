1644fe3f5ed2635d100bce6b447356c5eb338966
Michon van Dooren <me@maienm.com>
2025-12-01 23:08:40 +0100

    Implement 15-14-2

--- aoc/src/bin/15-14.rs
+++ aoc/src/bin/15-14.rs
@@ -1,5 +1,7 @@
 puzzle_lib::setup!(title = "Reindeer Olympics");
 
+use std::iter::repeat_n;
+
 use num::Integer;
 
 #[derive(Debug, Eq, PartialEq)]
@@ -28,7 +30,7 @@ fn parse_input(input: &str) -> Vec<Reindeer<'_>> {
     } => reindeer)
 }
 
-fn solve(reindeer: Vec<Reindeer>, elapsed: u16) -> u16 {
+fn part1impl(reindeer: Vec<Reindeer>, elapsed: u16) -> u16 {
     reindeer
         .into_iter()
         .map(|r| {
@@ -41,7 +43,47 @@ fn solve(reindeer: Vec<Reindeer>, elapsed: u16) -> u16 {
 
 pub fn part1(input: &str) -> u16 {
     let reindeer = parse_input(input);
-    solve(reindeer, 2503)
+    part1impl(reindeer, 2503)
+}
+
+fn part2impl(reindeer: Vec<Reindeer>, elapsed: u16) -> u16 {
+    let mut cycles: Vec<_> = reindeer
+        .into_iter()
+        .map(|r| {
+            repeat_n(r.speed, r.time as usize)
+                .chain(repeat_n(0, r.rest as usize))
+                .cycle()
+        })
+        .collect();
+    let mut distances: Vec<_> = cycles.iter().map(|_| 0).collect();
+    let mut points: Vec<_> = cycles.iter().map(|_| 0).collect();
+    for _ in 0..elapsed {
+        let mut max = 0;
+        for (i, step) in cycles
+            .iter_mut()
+            .map(Iterator::next)
+            .map(Option::unwrap)
+            .enumerate()
+        {
+            let distance = &mut distances[i];
+            *distance += step;
+            if *distance > max {
+                max = *distance;
+            }
+        }
+
+        for (i, distance) in distances.iter().enumerate() {
+            if *distance == max {
+                points[i] += 1;
+            }
+        }
+    }
+    points.into_iter().max().unwrap()
+}
+
+pub fn part2(input: &str) -> u16 {
+    let reindeer = parse_input(input);
+    part2impl(reindeer, 2503)
 }
 
 #[cfg(test)]
@@ -78,8 +120,14 @@ mod tests {
     }
 
     #[test]
-    fn example_solve() {
+    fn example_part1() {
         let reindeer = parse_input(&EXAMPLE_INPUT);
-        assert_eq!(solve(reindeer, 1000), 1120);
+        assert_eq!(part1impl(reindeer, 1000), 1120);
+    }
+
+    #[test]
+    fn example_part2() {
+        let reindeer = parse_input(&EXAMPLE_INPUT);
+        assert_eq!(part2impl(reindeer, 1000), 689);
     }
 }
