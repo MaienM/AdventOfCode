eeb0cd2f990e4805be1ff16600c0ecfb22981348
Michon van Dooren <me@maienm.com>
2025-12-07 06:20:27 +0100

    Implement 25-07-2

--- aoc/src/bin/25-07.rs
+++ aoc/src/bin/25-07.rs
@@ -16,13 +16,11 @@ fn parse_input(input: &str) -> (Grid, Point2) {
 
 pub fn part1(input: &str) -> usize {
     let (grid, start) = parse_input(input);
-    let width = grid.width();
-    let height = grid.height();
     let mut split = 0;
-    let mut beams: Vec<_> = (0..width).map(|_| false).collect();
+    let mut beams: Vec<_> = (0..grid.width()).map(|_| false).collect();
     beams[start.x] = true;
-    for y in 0..height {
-        for x in 0..width {
+    for y in 0..grid.height() {
+        for x in 0..grid.width() {
             if beams[x] && grid[Point2::new(x, y)] {
                 split += 1;
                 beams[x - 1] = true;
@@ -34,6 +32,23 @@ pub fn part1(input: &str) -> usize {
     split
 }
 
+pub fn part2(input: &str) -> usize {
+    let (grid, start) = parse_input(input);
+    let mut beams: Vec<_> = (0..grid.width()).map(|_| 0).collect();
+    beams[start.x] = 1;
+    for y in 0..grid.height() {
+        for x in 0..grid.width() {
+            let count = beams[x];
+            if count > 0 && grid[Point2::new(x, y)] {
+                beams[x - 1] += count;
+                beams[x] = 0;
+                beams[x + 1] += count;
+            }
+        }
+    }
+    beams.into_iter().sum()
+}
+
 #[cfg(test)]
 mod tests {
     use pretty_assertions::assert_eq;
@@ -41,7 +56,7 @@ mod tests {
 
     use super::*;
 
-    #[example_input(part1 = 21)]
+    #[example_input(part1 = 21, part2 = 40)]
     static EXAMPLE_INPUT: &str = "
         .......S.......
         ...............
