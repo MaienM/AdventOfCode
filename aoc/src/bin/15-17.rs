09f9d377a7675e001d5378ae9ba2a129a56391e1
Michon van Dooren <me@maienm.com>
2025-12-07 12:05:58 +0100

    Implement 15-17-2

--- aoc/src/bin/15-17.rs
+++ aoc/src/bin/15-17.rs
@@ -1,12 +1,14 @@
 puzzle_runner::register_chapter!(book = "2015", title = "No Such Thing as Too Much");
 
+use std::cmp::Ordering;
+
 fn parse_input(input: &str) -> Vec<u8> {
     parse!(input => {
         [nums split on '\n' as u8]
     } => nums)
 }
 
-fn find_options(containers: &[u8], left: u8) -> usize {
+fn count_options(containers: &[u8], left: u8) -> u16 {
     if left == 0 {
         return 1;
     }
@@ -14,22 +16,51 @@ fn find_options(containers: &[u8], left: u8) -> usize {
         return 0;
     }
 
-    let with_current = if let Some(left) = left.checked_sub(containers[0]) {
-        find_options(&containers[1..], left)
+    let without_current = count_options(&containers[1..], left);
+    if let Some(left) = left.checked_sub(containers[0]) {
+        without_current + count_options(&containers[1..], left)
     } else {
-        0
-    };
-    let without_current = find_options(&containers[1..], left);
-    with_current + without_current
+        without_current
+    }
 }
 
-fn part1impl(input: &str, liters: u8) -> usize {
+fn part1impl(input: &str, liters: u8) -> u16 {
     let containers = parse_input(input);
-    find_options(&containers, liters)
+    count_options(&containers, liters)
 }
 
-pub fn part1(input: &str) -> usize {
+pub fn part1(input: &str) -> u16 {
     part1impl(input, 150)
+}
+
+fn min_options(containers: &[u8], left: u8, used: u8) -> (u8, u16) {
+    if left == 0 {
+        return (used, 1);
+    }
+    if containers.is_empty() {
+        return (u8::MAX, 0);
+    }
+
+    let without_current = min_options(&containers[1..], left, used);
+    if let Some(left) = left.checked_sub(containers[0]) {
+        let with_current = min_options(&containers[1..], left, used + 1);
+        match with_current.0.cmp(&without_current.0) {
+            Ordering::Less => with_current,
+            Ordering::Equal => (with_current.0, with_current.1 + without_current.1),
+            Ordering::Greater => without_current,
+        }
+    } else {
+        without_current
+    }
+}
+
+fn part2impl(input: &str, liters: u8) -> u16 {
+    let containers = parse_input(input);
+    min_options(&containers, liters, 0).1
+}
+
+pub fn part2(input: &str) -> u16 {
+    part2impl(input, 150)
 }
 
 #[cfg(test)]
@@ -51,5 +82,10 @@ mod tests {
     #[test]
     fn example_input_part1() {
         assert_eq!(part1impl(&EXAMPLE_INPUT, 25), 4);
+    }
+
+    #[test]
+    fn example_input_part2() {
+        assert_eq!(part2impl(&EXAMPLE_INPUT, 25), 3);
     }
 }
