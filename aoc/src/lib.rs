eaceb93c340b94a45ed95325a66f0bc00112cba9
Michon van Dooren <me@maienm.com>
2025-12-08 23:08:01 +0100

    Move AoC input downloading to controller

--- aoc/src/lib.rs
+++ aoc/src/lib.rs
@@ -1,3 +1,60 @@
 //! My solutions to the [Advent of Code](https://adventofcode.com) challenges.
 
-puzzle_runner::register_series!(title = "Advent of Code");
+use std::{env, fs, sync::Arc};
+
+use puzzle_runner::{
+    controller::{Controller, ControllerResult},
+    derived::Chapter,
+};
+use reqwest::{
+    Url,
+    blocking::{Client, ClientBuilder},
+    cookie::Jar,
+};
+
+struct AoCController {
+    url: Url,
+    client: Client,
+}
+impl AoCController {
+    fn new() -> ControllerResult<Self> {
+        let url = "https://adventofcode.com".parse::<Url>()?;
+        let session = fs::read_to_string(env::var("AOC_SESSION_COOKIE_FILE")?)?;
+
+        let jar = Jar::default();
+        jar.add_cookie_str(&format!("session={session}"), &url);
+
+        let client = ClientBuilder::new()
+            .use_rustls_tls()
+            .cookie_provider(Arc::new(jar))
+            .build()?;
+
+        Ok(Self { url, client })
+    }
+
+    fn chapter_url(&self, chapter: &Chapter, stem: &str) -> ControllerResult<Url> {
+        let (year, day) = chapter.name.split_once('-').ok_or_else(|| {
+            format!(
+                "failed to parse year/day from chapter name {}",
+                chapter.name
+            )
+        })?;
+        let year = 2000 + year.parse::<i32>()?;
+        let day = day.parse::<u32>()?;
+
+        Ok(self.url.join(&format!("/{year}/day/{day}/{stem}"))?)
+    }
+}
+impl Controller for AoCController {
+    fn get_input(&self, chapter: &Chapter) -> ControllerResult<String> {
+        println!("Downloading input...");
+        let url = self.chapter_url(chapter, "input")?;
+        let text = self.client.get(url).send()?.error_for_status()?.text()?;
+        Ok(text.strip_suffix('\n').unwrap_or(&text).to_owned())
+    }
+}
+
+puzzle_runner::register_series!(
+    title = "Advent of Code",
+    controller = AoCController::new().unwrap(),
+);
