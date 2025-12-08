12c9129a05462d49ca40884204df3355f54c601c
Michon van Dooren <me@maienm.com>
2025-12-08 23:11:20 +0100

    WIP implement result validation for AoC

--- aoc/src/lib.rs
+++ aoc/src/lib.rs
@@ -1,11 +1,14 @@
 //! My solutions to the [Advent of Code](https://adventofcode.com) challenges.
+#![allow(unused)]
 
 use std::{env, fs, sync::Arc};
 
+use common_macros::hash_map;
 use puzzle_runner::{
-    controller::{Controller, ControllerResult},
+    controller::{Controller, ControllerError, ControllerResult},
     derived::Chapter,
 };
+use regex::Regex;
 use reqwest::{
     Url,
     blocking::{Client, ClientBuilder},
@@ -51,6 +54,58 @@ impl Controller for AoCController {
         let url = self.chapter_url(chapter, "input")?;
         let text = self.client.get(url).send()?.error_for_status()?.text()?;
         Ok(text.strip_suffix('\n').unwrap_or(&text).to_owned())
+    }
+
+    fn validate_result(
+        &self,
+        chapter: &Chapter,
+        part: &puzzle_runner::derived::Part,
+        result: &str,
+    ) -> ControllerResult<Result<(), String>> {
+        let regex_main = Regex::new(r"(?s)<main>.+?</main>")?;
+        let regex_tag = Regex::new(r"<[^>]+?>")?;
+
+        // let url = self.chapter_url(chapter, "answer")?;
+        // let params = hash_map![
+        //     "level" => part.num.to_string(),
+        //     "answer" => result.to_owned(),
+        // ];
+        // let html = self
+        //     .client
+        //     .post(url)
+        //     .form(&params)
+        //     .send()?
+        //     .error_for_status()?
+        //     .text()?;
+
+        // let mut path = env::temp_dir();
+        // path.push("aoc-reponse.html");
+        // fs::write(&path, &html);
+
+        let html = fs::read_to_string("/home/maienm/Downloads/incorrect.html")?;
+        let main = regex_main
+            .find(&html)
+            .ok_or_else(|| {
+                let mut path = env::temp_dir();
+                path.push("aoc-reponse.html");
+                match fs::write(&path, &html) {
+                    Ok(()) => format!(
+                        "unable to find main section in response, see {}",
+                        path.display()
+                    ),
+                    // this'll be messy, but it beats not having the response available at all.
+                    Err(_) => format!("unable to find main section in response:\n\n{html}"),
+                }
+            })?
+            .as_str();
+        let text = regex_tag.replace_all(main, "");
+
+        println!("{text}");
+        if text.contains("That's the right answer!") {
+            Ok(Ok(()))
+        } else {
+            Ok(Err("incorrect answer".to_owned()))
+        }
     }
 }
 
