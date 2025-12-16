af474f871cac9227c16f73da1256028ba8ca2129
Michon van Dooren <me@maienm.com>
2025-12-16 18:28:35 +0100

    Use ehttp (instead of ureq) for AoC controller
    This is done for WASM support. I don't intend to use any of the current
    features of the controller in the web version (I have no desire to turn
    this into a one-click solve-this-for-me thing), but I do intend to add
    more features to the controller, some of which will be useful in the web
    version.

--- aoc/src/bin/controller.rs
+++ aoc/src/bin/controller.rs
@@ -1,5 +1,7 @@
 use std::{env, fs};
 
+use ehttp::{Request, fetch_async};
+use pollster::FutureExt;
 use puzzle_runner::controller::{Controller, ControllerResult};
 
 #[puzzle_runner::register_controller]
@@ -45,12 +47,11 @@ impl Controller for AoCController {
 
     fn get_input(&self, chapter: &str) -> ControllerResult<String> {
         let url = self.chapter_url(chapter, "input")?;
-        let text = ureq::get(url)
-            .header("cookie", self.cookie.clone())
-            .call()?
-            .body_mut()
-            .read_to_string()?;
-        Ok(text.strip_suffix('\n').unwrap_or(&text).to_owned())
+        let mut request = Request::get(url);
+        request.headers.insert("cookie", self.cookie.clone());
+        let response = fetch_async(request).block_on()?;
+        let text = response.text().ok_or("empty response")?;
+        Ok(text.strip_suffix('\n').unwrap_or(text).to_owned())
     }
 
     fn validate_result_impl(
@@ -60,17 +61,16 @@ impl Controller for AoCController {
         result: &str,
     ) -> ControllerResult<(bool, String)> {
         let url = self.chapter_url(chapter, "answer")?;
-        let html = ureq::post(url)
-            .header("cookie", self.cookie.clone())
-            .send_form([("level", part.to_string()), ("answer", result.to_owned())])?
-            .body_mut()
-            .read_to_string()?;
+        let mut request = Request::post(url, format!("level={part}&answer={result}").into_bytes());
+        request.headers.insert("cookie", self.cookie.clone());
+        let response = fetch_async(request).block_on()?;
+        let html = response.text().ok_or("empty response")?;
 
         let mut path = env::temp_dir();
         path.push("aoc-reponse.html");
-        let tmp_write = fs::write(&path, &html);
+        let tmp_write = fs::write(&path, html);
 
-        let text = AoCController::extract_text(&html).map_err(|e1| match tmp_write {
+        let text = AoCController::extract_text(html).map_err(|e1| match tmp_write {
             Ok(()) => format!("Failed to parse response ({}), wrote full response to {}.", String::from(e1), path.display()),
             Err(e2) => format!("Failed to parse response ({}), and failed to write response to temporary file {} ({e2}), so including full response here: {html}", String::from(e1), path.display())
         })?;
