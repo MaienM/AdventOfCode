5e6962560dbb88261c56f55b8205754065260616
Michon van Dooren <me@maienm.com>
2025-12-16 18:20:50 +0100

    Check that the controller is in bin/controller.rs

--- puzzle_runner/derive/src/register_controller.rs
+++ puzzle_runner/derive/src/register_controller.rs
@@ -2,11 +2,19 @@ use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::{ItemStruct, parse_macro_input};
 
-use crate::utils::source_crate;
+use crate::utils::{find_crate_root, return_err, source_call_site, source_crate};
 
 pub fn main(input: TokenStream, annotated_item: TokenStream) -> TokenStream {
     let args_parser = syn::meta::parser(|meta| Err(meta.error("unsupported property")));
     parse_macro_input!(input with args_parser);
+
+    if let Some(path) = source_call_site().local_file()
+        && let Ok(path) = path.canonicalize()
+        && let Ok(root) = find_crate_root(&path)
+        && path != root.join("src/bin/controller.rs")
+    {
+        return_err!(Err("must be used in src/bin/controller.rs"));
+    }
 
     let item = parse_macro_input!(annotated_item as ItemStruct);
     let ident = &item.ident;
