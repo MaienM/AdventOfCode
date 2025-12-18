4538852a5fe68c5f68b6e41904756d0acc3f70dc
Michon van Dooren <me@maienm.com>
2025-12-18 18:14:34 +0100

    Implement 15-20-1

---
+++ aoc/src/bin/15-20.rs
@@ -0,0 +1,26 @@
+puzzle_runner::register_chapter!(book = 2015, title = "Infinite Elves and Infinite Houses");
+
+#[register_part]
+fn part1(input: &str) -> usize {
+    let threshold: usize = input.parse().unwrap();
+    let elfsum = threshold / 10;
+    (0..usize::MAX)
+        .into_par_iter()
+        .by_exponential_blocks()
+        .find_first(|num| (1..=(num / 2)).filter(|i| num % i == 0).sum::<usize>() + num >= elfsum)
+        .unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+    use puzzle_runner::example_input;
+
+    use super::*;
+
+    #[example_input(part1 = 6)]
+    static EXAMPLE_INPUT_1: &str = "100";
+
+    #[example_input(part1 = 8)]
+    static EXAMPLE_INPUT_2: &str = "150";
+}
